003f354fa6c8ccdde81bcdd9c3838a40eef3499b
sani38 <saniyakurbanova6@gmail.com>
2026-05-27 14:02:49 +0500

    кандидаты и соискатели

---
+++ InternshipPractice.Application/Interfaces/Repositories/IEmployerApplicationRepository.cs
@@ -0,0 +1,15 @@
+﻿using InternshipPractice.Application.Responses;
+using KDS.Primitives.FluentResult;
+
+namespace InternshipPractice.Application.Interfaces.Repositories;
+
+public interface IEmployerApplicationRepository
+{
+    Task<Result<PagedResult<EmployerApplicationResponse>>> GetEmployerApplications(
+        Guid userId,
+        string? statusCode,
+        string lang,
+        int page,
+        int pageSize,
+        CancellationToken cancellationToken);
+}
---
+++ InternshipPractice.Application/Queries/GetEmployerApplications/GetEmployerApplicationsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using InternshipPractice.Application.Interfaces.Repositories;
+using InternshipPractice.Application.Responses;
+using KDS.Primitives.FluentResult;
+using MediatR;
+
+namespace InternshipPractice.Application.Queries.GetEmployerApplications;
+
+public class GetEmployerApplicationsQueryHandler(IEmployerApplicationRepository employerApplicationRepository)
+    : IRequestHandler<GetEmployerApplicationsQuery, Result<PagedResult<EmployerApplicationResponse>>>
+{
+    public async Task<Result<PagedResult<EmployerApplicationResponse>>> Handle(
+        GetEmployerApplicationsQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await employerApplicationRepository.GetEmployerApplications(
+            request.UserId,
+            request.StatusCode,
+            request.Lang,
+            request.Page,
+            request.PageSize,
+            cancellationToken);
+    }
+}
