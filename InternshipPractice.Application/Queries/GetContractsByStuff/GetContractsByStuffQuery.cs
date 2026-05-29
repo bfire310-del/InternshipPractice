6ae8b8d448ec0acfdec5cf2b9942f392fbcbecc8
Malika <malika.andassova@gmail.com>
2026-05-29 15:03:17 +0500

    [add] Подписание со стороны работадателя и университета

---
+++ InternshipPractice.Application/Queries/GetContractsByStuff/GetContractsByStuffQuery.cs
@@ -0,0 +1,12 @@
+﻿using InternshipPractice.Application.Responses;
+using KDS.Primitives.FluentResult;
+using MediatR;
+
+namespace InternshipPractice.Application.Queries.GetContractsByUserId;
+
+public record GetContractsByUserIdQuery(
+    Guid UserId,
+    string Lang,
+    int Page,
+    int PageSize
+) : IRequest<Result<PagedResult<ContractResponse>>>;
---
+++ InternshipPractice.Application/Queries/GetContractsByStuff/GetContractsByStuffQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using InternshipPractice.Application.Interfaces.Repositories;
+using InternshipPractice.Application.Responses;
+using KDS.Primitives.FluentResult;
+using MediatR;
+
+namespace InternshipPractice.Application.Queries.GetContractsByUserId;
+
+public class GetContractsByUserIdQueryHandler(IContractRepository contractRepository) : IRequestHandler<GetContractsByUserIdQuery, Result<PagedResult<ContractResponse>>>
+{
+    public async Task<Result<PagedResult<ContractResponse>>> Handle(GetContractsByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await contractRepository.GetContractsByUserId(request.UserId, request.Lang, request.Page, request.PageSize);
+        return result;
+    }
+}
