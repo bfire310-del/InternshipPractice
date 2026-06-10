51458628fbffb6fa66afe6da3b7bc3226da2d4ef
studentAskar <estai7735@gmail.com>
2026-06-10 05:01:57 +0500

    add fio

--- InternshipPractice.Application/Queries/Login/LoginQueryHandler.cs
+++ InternshipPractice.Application/Queries/Login/LoginQueryHandler.cs
@@ -6,15 +6,15 @@ using MediatR;
 
 namespace InternshipPractice.Application.Queries.Login;
 
-public class LoginQueryHandler(IAccessHttpClient accessHttpClient) : IRequestHandler<LoginQuery, Result<(string, string)>>
+public class LoginQueryHandler(IAccessHttpClient accessHttpClient) : IRequestHandler<LoginQuery, Result<(string, string, string, string)>>
 {
-    public async Task<Result<(string, string)>> Handle(LoginQuery request, CancellationToken cancellationToken)
+    public async Task<Result<(string, string, string, string)>> Handle(LoginQuery request, CancellationToken cancellationToken)
     {
         var result = await accessHttpClient.Login(request.Email, request.Password);
 
         if (result.IsFailed)
-            return Result.Failure<(string,string)>(result.Error);
+            return Result.Failure<(string,string, string, string)>(result.Error);
 
-        return Result.Success((result.Value.Token, result.Value.RoleCode));
+        return Result.Success((result.Value.Token, result.Value.RoleCode, result.Value.FirstName, result.Value.LastName));
     }
 }
--- InternshipPractice.Domain/Dto/LoginResponseDto.cs
+++ InternshipPractice.Domain/Dto/LoginResponseDto.cs
@@ -4,4 +4,6 @@ public class LoginResponseDto
 { 
     public string Token { get; set; }
     public string RoleCode { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
